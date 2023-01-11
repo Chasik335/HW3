831c9656e336524dc80d4ff7028ecd3231678b99
Vladislav <vhasovnikov@inbox.ru>
2023-01-11 20:28:41 +0500

    Написал код для 3-х задач, добавил инфу и кнопки в файл readme.md

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+void Polindrom(int number)
+{
+  if (number / 10000 == number % 10)
+  {
+      int newNumber = number / 1000 % 10;
+      number = number / 10 % 10;
+      if (newNumber == number)
+      {
+          Console.WriteLine("Число палиндром");
+      }
+      else Console.WriteLine("Число не палиндром");
+  }
+  else Console.WriteLine("Число не палиндром");
+}
+int ValueInput()
+{
+    Console.Write("Введите пятизнаяное число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int number = ValueInput();
+Polindrom(number);
---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти кубы чисел от 1 до N
+int ValueInput()
+{
+    Console.Write("Введите число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+void cubeNumber(int number)
+{
+    
+    for (int i = 1; i <= number; i++)
+    {
+        double cube = Math.Pow (i, 3);
+        Console.WriteLine($"Куб числа {i} = " + cube);
+    } 
+}
+int number = ValueInput();
+cubeNumber(number);
